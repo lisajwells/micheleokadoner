7ec65c85d9ccae6c06e7949def11a8371532afcd
Lisa Wells <lisawells999@gmail.com>
2018-01-22 12:02:37 -0500

    experiment with alternate no-header no-footer template

--- blackpagetemp.php
+++ blackpagetemp.php
@@ -5,28 +5,28 @@
 
  */
 
-get_header();
+get_header('blackpage');
 ?>
-	
+
 <!-- #Content -->
 <div id="Content">
 	<div class="content_wrapper clearfix">
 
 		<!-- .sections_group -->
 		<div class="sections_group">
-		
+
 			<div class="entry-content" itemprop="mainContentOfPage">
-			
-				<?php 
+
+				<?php
 					while ( have_posts() ){
 						the_post();							// Post Loop
 						mfn_builder_print( get_the_ID() );	// Content Builder & WordPress Editor Content
 					}
 				?>
-				
+
 				<div class="section section-page-footer">
 					<div class="section_wrapper clearfix">
-					
+
 						<div class="column one page-pager">
 							<?php
 								// List of pages
@@ -39,32 +39,32 @@ get_header();
 								));
 							?>
 						</div>
-						
+
 					</div>
 				</div>
-				
+
 			</div>
-			
+
 			<?php if( mfn_opts_get('page-comments') ): ?>
 				<div class="section section-page-comments">
 					<div class="section_wrapper clearfix">
-					
+
 						<div class="column one comments">
 							<?php comments_template( '', true ); ?>
 						</div>
-						
+
 					</div>
 				</div>
 			<?php endif; ?>
-	
+
 		</div>
-		
+
 		<!-- .four-columns - sidebar -->
 		<?php get_sidebar(); ?>
 
 	</div>
 </div>
 
-<?php get_footer();
+<?php get_footer('bbb');
 
 // Omit Closing PHP Tags
