772ab317d48700e32886661b0c69f24c1d480b02
Lisa Wells <lisawells999@gmail.com>
2018-06-29 07:59:30 -0400

    prductpage-masonry: add image-box class to items for decade filter

--- prductpage-masonry.php
+++ prductpage-masonry.php
@@ -75,7 +75,7 @@ get_header();
 							};
 
 							?>
-                            <div class="masonry_img_box grid-item <?php echo $label . ' ';
+                            <div class="image-box masonry_img_box grid-item <?php echo $label . ' ';
 							echo $itemwidth; ?>">
                                 <a href="#/icon-gallery/<?php echo esc_attr( $post->ID ) ?>" class="box-link">
 
